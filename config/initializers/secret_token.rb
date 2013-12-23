41bd9dde1294c2efef71780e0b0e8f552280ff5d
pkrawat1 <pankajrawat19sept@gmail.com>
2013-12-23 10:45:57 +0530

    topbar created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aitpace::Application.config.secret_key_base = '76527e677372ec22b6aaabf3f91cb6e7d3a2d1f822c65b0319ebd8eb43c9ddbede53061948c3bef331429b6b900b8fbe1f86c98c36d5b4a3d35c6c5c9d533845'
