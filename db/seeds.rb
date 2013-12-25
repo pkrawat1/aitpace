17a99c7c409a7e9af2cd80cdbc9dc166fbacc2ab
pkrawat1 <pankajrawat19sept@gmail.com>
2013-12-25 11:56:13 +0530

    database added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Events.delete_all
 Events.create(name:"Cricket", fees: 900, rules: "Play safe")
 Events.create(name:"Football", fees: 1100, rules: "Play safe")
 Events.create(name:"Tennis", fees: 300, rules: "Play safe")
 Events.create(name:"Badminton", fees: 300, rules: "Play safe")
 Events.create(name:"Basketball", fees: 900, rules: "Play safe")
 Events.create(name:"Volleyball", fees: 500, rules: "Play safe")
+Events.create(name:"Table Tennis", fees: 200, rules: "Play safe")
+Events.create(name:"Chess", fees: 200, rules: "Play safe")
+Events.create(name:"Squash", fees: 500, rules: "Play safe")
