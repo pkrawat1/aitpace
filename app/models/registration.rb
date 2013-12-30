61fa4cef3212f5db03f532762eb825bb7f6c5bce
pkrawat1 <pankajrawat19sept@gmail.com>
2013-12-30 23:24:02 +0530

    validations added

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,2 +1,6 @@
 class Registration < ActiveRecord::Base
+  validates :name, :mob_no, :email, :branch, :year, :college, :event, :members, :staff_name, :staff_contact, :staff_email, presence: :true
+  validates :mob_no, :staff_contact, length: {is: 10}, numericality: { only_integer: true, greater_than: 0}
+  validates :email, :staff_email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :name, :members, :staff_name, numericality: false
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ Aitpace::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  I18n.enforce_available_locales = false
 end
