0fdf6b9b37dd841e3e4975fd56a02aff6aad9cb1
pkrawat1 <pankajrawat19sept@gmail.com>
2014-01-11 14:31:32 +0530

    using event name in url intead of id's

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -5,15 +5,15 @@ class EventController < ApplicationController
   end
 
   def show
-    @event = Events.find(params[:id])
+    @event = Events.find_by_name(params[:id])
   end
 
   def edit
-    @event = Events.find(params[:id])
+    @event = Events.find_by_name(params[:id])
   end
 
   def update
-    @event = Events.find(params[:id])
+    @event = Events.find_by_name(params[:id])
     if @event.update(params[:event].permit!)
       redirect_to event_path(params[:id])
     else
