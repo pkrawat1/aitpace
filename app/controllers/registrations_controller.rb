bd54d772c22c792bd430a441b2316c87f44e8c2c
pkrawat1 <pankajrawat19sept@gmail.com>
2014-01-12 23:41:11 +0530

    event and year dropdown added in registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 class RegistrationsController < ApplicationController
   def new
     @registration = Registration.new
+    @event_names = []
+    @events.select(:name).each{|x|  @event_names << x.name}
   end
 
   def create
