761524645e0443f27b68760f727eef1e6c2731ca
pkrawat1 <pankajrawat19sept@gmail.com>
2014-01-15 02:35:42 +0530

    updated gem file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //=require foundation/foundation
 //=require foundation/foundation.orbit
